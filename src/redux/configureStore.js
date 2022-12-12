9413f3bfaf6c87215e2f81eb31c216fe3ecc1fa9
Robert Escobedo <robert.escobedo@pharr-tx.gov>
2022-12-12 15:31:57 -0600

    added state to Login.js, added store for redux

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import kpiDataMemoryReducer from "./ducks/stats";
 
 const store = configureStore({
   reducer: {
     // bring in reducers here
+    kpiDataMemory: kpiDataMemoryReducer
   },
 });
 export default store;
